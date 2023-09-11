ed7b57aa43913038fdd2d2b5c2b3ffbe8f917c65
BradyLange1 <bradylange99@gmail.com>
2023-09-11 16:44:37 -0500

    added models and routes

---
+++ controllers/userController.js
@@ -0,0 +1,82 @@
+const { ObjectId } = require('mongoose').Types;
+const { User } = require("../models");
+
+module.exports = {
+  //get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //get a single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.find(req.params.userId);
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //create a user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(req.body);
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //add a friend
+  async addFriend(req, res) {
+    try {
+      const friend = await User.create(req.body);
+
+      res.json(friend);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //delete a friend
+  async deleteFriend(req, res) {
+    try {
+      const user = await User.findOneAndRemove({ _id: req.params.friendId });
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
