350b2b6cebf874468ae1cb969b3fd8c9ffdae207
BradyLange1 <bradylange99@gmail.com>
2023-09-12 14:16:18 -0500

    added bonus challenge

--- controllers/userController.js
+++ controllers/userController.js
@@ -51,6 +51,7 @@ module.exports = {
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndRemove({ _id: req.params.userId });
+      await Thought.deleteMany({username: user.username})
       res.json(user);
     } catch (err) {
       console.log(err);
